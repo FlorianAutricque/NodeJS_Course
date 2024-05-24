00df0569116e26b01ed2e2cdaf465b720b3592c5
florian autricque <fautricque@gmail.com>
2024-05-24 12:44:24 -0400

    first lesson

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,2 +1,13 @@
-const hello = "Hello world";
-console.log(hello);
+// fs is file system
+const fs = require("fs");
+
+//needs 2 arguments, first one is what we want to read (path)
+
+//read a file
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+//write-create a file
+const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written");
