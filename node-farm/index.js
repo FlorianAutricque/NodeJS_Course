527ef24ba6f3c2a3dd26c975023919bd05a2a3c7
florian autricque <fautricque@gmail.com>
2024-05-24 13:11:44 -0400

    callback hell

--- node-farm/index.js
+++ node-farm/index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 //needs 2 arguments, first one is what we want to read (path)
 
+//BLOCKING, SYNCHRONOUS WAY
 //read a file
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
@@ -11,3 +12,18 @@ console.log(textIn);
 const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written");
+
+//NON-BLOCKING, ASYNCHRONOUS WAY
+
+//callback hell
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("file has been written");
+      });
+    });
+  });
+});
