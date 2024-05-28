4262a2f2bed54ae4c6403aae43a776baf0b15504
florian autricque <fautricque@gmail.com>
2024-05-28 12:49:10 -0400

    promises

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -1,7 +1,47 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+//Promise
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not load the file");
+      resolve("sucess");
+    });
+  });
+};
+
+//we can chain up 'then' because everytime it returns a promise
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog image saved to file");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 //__dirname is used to get the absolute path to the directory containing the script.
+
+//that is an example of callback hell:
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
