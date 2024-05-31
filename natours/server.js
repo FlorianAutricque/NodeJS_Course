ff3b0e1e3bd50fb2fb6c44298a856bd361304312
florian autricque <fautricque@gmail.com>
2024-05-31 12:59:11 -0400

    connection to atlasd

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,26 @@
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  //connect to atlas
+  .connect(DB, {
+    //connect to local db
+    // .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connection success");
+  });
 
 const app = require("./app");
 //////START SERVER
