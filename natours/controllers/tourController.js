8e65404be62d2bdede1da63280300dba90dadb62
florian autricque <fautricque@gmail.com>
2024-05-31 16:22:20 -0400

    read doc

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -4,22 +4,42 @@ const Tour = require("../models/tourModel");
 
 //TOUR
 //READ all
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "sucess",
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours: tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 //READ one
-exports.getTour = (req, res) => {
-  console.log(req.params);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  const id = req.params.id * 1;
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 //CREATE
