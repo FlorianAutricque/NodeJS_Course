260b810572caf3516b181830e30eefa3010838b1
florian autricque <fautricque@gmail.com>
2024-05-31 19:07:20 -0400

    import and delete data

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connection success");
+  });
+
+//READ JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+//IMPORT DATA TO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data successfully loaded");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//IMPORT DELETE DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
+
+/*
+
+run that to delete all data in db
+ node dev-data/data/import-dev-data.js --delete
+
+ run that to import all data in db
+ node dev-data/data/import-dev-data.js --import
+
+ */
