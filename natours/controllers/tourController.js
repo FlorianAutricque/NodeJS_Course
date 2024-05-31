fa3c442acd673a053b00fef94ce6ea272aeb0ae6
florian autricque <fautricque@gmail.com>
2024-05-31 16:51:16 -0400

    update

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -62,13 +62,25 @@ exports.createTour = async (req, res) => {
 };
 
 //UPDATE
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: ">Updated tour here...>",
-    },
+exports.updateTour = async (req, res) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
   });
+
+  try {
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 //DELETE
