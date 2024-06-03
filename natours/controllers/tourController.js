05bb4d6b5025c28b4ee7cd1ab2bff1404ed1f542
florian autricque <fautricque@gmail.com>
2024-06-03 15:06:50 -0400

    advanced filtering

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -7,11 +7,16 @@ const Tour = require("../models/tourModel");
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
+    //Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lt|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // const tours = await Tour.find()
     //   .where("duration")
