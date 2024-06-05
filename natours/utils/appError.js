0d1fd48b30fb81bc313877e8f6719d128b2d0258
florian autricque <fautricque@gmail.com>
2024-06-05 15:58:58 -0400

    handling errors

---
+++ natours/utils/appError.js
@@ -0,0 +1,15 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    //call the parent constructor: use super
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
