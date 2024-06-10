822b1a8e0d8f4313787f6f32a1cb220950aa759a
florian autricque <fautricque@gmail.com>
2024-06-10 13:32:27 -0400

    creating user schema

---
+++ natours/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator, isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm tour password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
