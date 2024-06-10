131b17a42ea6f594ca5eab357e2f301a93bac447
florian autricque <fautricque@gmail.com>
2024-06-10 15:04:38 -0400

    hash password

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,28 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm tour password"],
+    validate: {
+      //This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
   },
+});
+
+//HASH PASSWORD
+userSchema.pre("save", async function (next) {
+  //Only run this function if password was modified
+  if (!this.isModified("password")) return next();
+
+  // hash = encrypted
+  // Hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
