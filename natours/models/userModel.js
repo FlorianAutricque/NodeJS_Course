d3b82aee896b3402e621a68a5ab2a80e0fabceab
florian autricque <fautricque@gmail.com>
2024-06-11 15:22:55 -0400

    reset token

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
@@ -39,6 +40,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 //HASH PASSWORD
@@ -75,6 +78,22 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   //False means not changed
   return false;
+};
+
+//CHANGE PASSWORD
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model("User", userSchema);
