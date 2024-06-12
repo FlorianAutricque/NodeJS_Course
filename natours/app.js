ce82a1a328b5a270f7ef9a120ab03f9b7fff4438
florian autricque <fautricque@gmail.com>
2024-06-12 12:47:49 -0400

    rate limit

--- natours/app.js
+++ natours/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -11,9 +12,18 @@ const app = express();
 
 //middleware ; passerelle entre les autres applications, outils et bases de données pour offrir aux utilisateurs des services unifiés
 
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
 
 app.use(express.json());
 
