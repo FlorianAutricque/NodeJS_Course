6f796465e52443b9705caa7054153147927b985b
florian autricque <fautricque@gmail.com>
2024-06-17 15:29:59 -0400

    populating

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 // const validator = require("validator");
-const User = require("./userModel");
+// const User = require("./userModel");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -125,12 +125,12 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-tourSchema.pre("save", async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
+// tourSchema.pre("save", async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 
-  next();
-});
+//   next();
+// });
 
 // tourSchema.pre("save", function (next) {
 //   console.log("Will save doc");
@@ -147,6 +147,15 @@ tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
 
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+
   next();
 });
 
