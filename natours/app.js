f319df934f1b2e200767034a315c4cdf7abaefe2
florian autricque <fautricque@gmail.com>
2024-06-19 16:14:27 -0400

    pug

--- natours/app.js
+++ natours/app.js
@@ -74,7 +74,10 @@ app.use((req, res, next) => {
 //ROUTES
 //SERVER SIDE RENDER
 app.get("/", (req, res) => {
-  res.status(200).render("base");
+  res.status(200).render("base", {
+    tour: "The Forest Hiker",
+    user: "Florian",
+  });
 });
 
 //API
