cc453f962fe26e9af64fd6d53f2f4cf785816b24
florian autricque <fautricque@gmail.com>
2024-06-19 19:26:48 -0400

    project structure

--- natours/app.js
+++ natours/app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
 
@@ -72,27 +73,9 @@ app.use((req, res, next) => {
 });
 
 //ROUTES
-//SERVER SIDE RENDER
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Florian",
-  });
-});
-
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All Tours",
-  });
-});
-
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The forst hiker",
-  });
-});
 
 //API
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
