1049c90605f0bde6985f02822b476cfbf336a9fd
florian autricque <fautricque@gmail.com>
2024-06-21 10:31:08 -0400

    fixing bug

--- natours/app.js
+++ natours/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitaze = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -27,7 +28,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Security HTTP headers
-app.use(helmet());
+// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Development login
 if (process.env.NODE_ENV === "development") {
@@ -45,6 +47,7 @@ app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitaze());
@@ -69,6 +72,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
