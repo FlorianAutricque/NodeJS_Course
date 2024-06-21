511e3076c37ed7777b3cab41f71252a874c7f0ed
florian autricque <fautricque@gmail.com>
2024-06-20 20:20:10 -0400

    bug

--- natours/app.js
+++ natours/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitaze = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -45,6 +46,7 @@ app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitaze());
