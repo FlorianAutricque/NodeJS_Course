8552e66b402174f89af7ef27ec49a17986629b5c
florian autricque <fautricque@gmail.com>
2024-06-21 10:44:43 -0400

    solve bug

--- natours/app.js
+++ natours/app.js
@@ -28,7 +28,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Security HTTP headers
-app.use(helmet());
+// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Development login
 if (process.env.NODE_ENV === "development") {
@@ -71,6 +72,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
