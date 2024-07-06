35245c2da40c739419ddbcd48a9f461a13122857
florian autricque <fautricque@gmail.com>
2024-07-05 21:07:29 -0400

    change stripe to make it secure

--- natours/routes/viewRoutes.js
+++ natours/routes/viewRoutes.js
@@ -5,7 +5,10 @@ const bookingController = require("../controllers/bookingController");
 
 const router = express.Router();
 
+router.use(viewsController.alerts)
+
 router.get("/", authController.isLoggedIn, viewsController.getOverview);
+
 router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
 router.get("/login", authController.isLoggedIn, viewsController.getLogin);
 router.get("/signup", authController.isLoggedIn, viewsController.getSignup);
